d93f5aff0962ac93af98a8a214dbc2684e833c95
Timothy Gibson <ryaninlux@gmail.com>
2022-06-02 16:31:14 -0700

    Add select box, input box, and button to html

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyConversion from './js/conversion.js';
+import { CurrencyConversion } from './js/conversion.js';
 
-
-$(document).ready(function() {
-  $('#').submit(function(event) {
-    event.preventDefault();
-
-  });
-});
