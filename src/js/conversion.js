85397b06a94c778f388ab5a402b7d68282ba15bb
Timothy Gibson <ryaninlux@gmail.com>
2022-06-02 17:05:19 -0700

    Add api function CurrencyConversion class in conversion.js

--- src/js/conversion.js
+++ src/js/conversion.js
@@ -1,8 +1,14 @@
 export default class CurrencyConversion {
-  constructor() {
-    
-  }
-  method() {
-  
+  static getCurrency(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/37e98494cb10338ff8261fb2/latest/USD`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
