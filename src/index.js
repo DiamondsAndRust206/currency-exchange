2b185fc9505529d33d02c8e0be5329c106440c4a
Timothy RYAN <ryaninlux@gmail.com>
2022-06-11 14:35:31 -0700

    Finish the if else statment in async function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ async function convertCurrency() {
   let currency;
   if (currencyType === "pnd") {
     currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/GBP/${amount}`;
+  } else if (currencyType === "euro") {
+    currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/EUR/${amount}`;
+  } else if (currencyType === "yen") {
+    currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/JPY/${amount}`;
+  } else if (currencyType === "aus") {
+    currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/AUD/${amount}`;
+  } else if (currencyType === "can") {
+    currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/CAD/${amount}`;
 }
 
 $(document).ready(function () {
