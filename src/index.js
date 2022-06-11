a5bb63456003025ee6af3497cc2dc2667b327820
Timothy RYAN <ryaninlux@gmail.com>
2022-06-11 15:25:50 -0700

    Add error function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { CurrencyConversionApi } from './js/conversion.js';
 
+function errorFunction(element, response) {
+  if (response instanceof Error) {
+    $(element).text("");
+    $(element).append("<p class='error'>There was an error getting this data from api<p>");
+    return true;
+  } else {
+    return false;
+  }
+}
+
 async function convertCurrency() {
   let amount = $("#number-input").val();
   const currencyType = $("input:radio[name=operator]:checked").val();
@@ -18,6 +28,9 @@ async function convertCurrency() {
     currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/AUD/${amount}`;
   } else if (currencyType === "can") {
     currency = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/CAD/${amount}`;
+  } else {
+    $("#error-message").text("this is not a supported currency!")
+  }
 }
 
 $(document).ready(function () {
